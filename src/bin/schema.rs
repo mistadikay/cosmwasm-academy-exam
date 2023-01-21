e8d5b900722643c82fe0eb33b462dbbdd9f042a4
Denis Koltsov <iam@mistadikay.com>
2023-01-21 23:51:16 +0100

    Fix incorrect reference in schema

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,5 +1,5 @@
+use cosmwasm_bidding::msg::{ExecMsg, InstantiateMsg, QueryMsg};
 use cosmwasm_schema::write_api;
-use counting_contract::msg::{ExecMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
